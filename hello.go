49748297e27b6938ecc89c5fc076126f1cd4c656
Jens Hilligsøe <jens@hilli.dk>
2020-02-03 13:27:50 +0100

    Say Hello To variadic

--- hello.go
+++ hello.go
@@ -1,8 +1,16 @@
 package hello
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // SayHello just says Hello
 func SayHello() {
 	fmt.Println("Hello")
+}
+
+// SayHelloTo says hello to all present
+func SayHelloTo(names ...string) {
+	fmt.Println("Hello", strings.Join(names, ", "))
 }
